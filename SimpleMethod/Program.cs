b6d607754d4a21e4f61451f462d6f2ae46cc8289
HarveySembhy <h.sembhy@gmail.com>
2018-09-05 10:42:01 +0100

    Simpliest way to invoke a method - learning by typing in the code itself as you are following the tutorial - learn much better and feel relaxed whiel learning.

--- SimpleMethod/Program.cs
+++ SimpleMethod/Program.cs
@@ -10,6 +10,14 @@ namespace SimpleMethod
     {
         static void Main(string[] args)
         {
+            HelloWorld();
+            Console.ReadLine();
         }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
     }
 }
